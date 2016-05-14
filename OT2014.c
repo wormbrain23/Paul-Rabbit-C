761459f45e832527c69489fce126689f6d5c419d
wormbrain23 <tradekcah@gmail.com>
2016-05-14 13:44:27 +0200

    dorobenie zvysku

--- OT2014.c
+++ OT2014.c
@@ -10,17 +10,19 @@
  * a str = "bozena" zostane v poli len {"andrea","blazena"} a funkcia vrati 2.
  * HINT: pri porovnavani vyuzite standardnu funkciu na porovnavanie retazcov,
  * pri vynechavani retazcov si uvedomte ze pracujete s polom smernikov. */
-/* TODO: po skonceni funkcie su v poli stale aj zvysne retazce */
 int funkciaOA1(char *pole[], char *str, int dlzka_pola) {
     int i=0,j=0,nova_dlzka=0;
     while (i < dlzka_pola) {
         if (strcmp(pole[i],str) < 0) {
+            /* retazce, ktore podmienku splnaju sa poposuvaju na zaciatok */
             pole[j] = pole[i];
             j++;
             nova_dlzka++;
         }
         i++;
     }
+    while (j < dlzka_pola)
+        pole[j++] = NULL; /*zvysne retazce budu prazdne */
     return nova_dlzka;
 }
 
@@ -31,9 +33,9 @@ int funkciaOA1(char *pole[], char *str, int dlzka_pola) {
 char *funkciaOA2() {
     char *riadok, znak;
     int dlzka_riadku=1, index=0;
-    znak = getc(stdin);
+    znak = getc(stdin); /* stdin - standardny vstup */
     riadok = malloc(dlzka_riadku);
-    if (znak == '\n') {
+    if (znak == '\n') { /* ak je koniec riadku hned ako prvy znak */
         riadok[0] = '\0';
         return riadok;
     }
@@ -52,7 +54,6 @@ char *funkciaOA2() {
 #define N 3
 int funkciaOA3(int matica[N][N]) {
     int i=0, j=0, max = 0, sucet = 0, index = 1;
-    
     /* na zaciatku je do maxima ulozeny sucet prveho riadku */
     for (j=0;j<N;j++)
         max += matica[0][j];
@@ -78,7 +79,7 @@ double funkciaOA4(char *nazov) {
     double sucet=0.0, priemer=0.0, akt_hodnota=0.0;
     int pocet=0;
     file = fopen(nazov, "r");
-    if (file == NULL) {
+    if (file == NULL) { /* fopen vracia NULL ak nastane chyba */
         printf("Subor sa nepodarilo otvorit.\n");
         exit(1);
     }
@@ -86,7 +87,7 @@ double funkciaOA4(char *nazov) {
         pocet++;
         sucet += akt_hodnota;
     }
-    if (fclose(file) != 0) {
+    if (fclose(file) != 0) { /* pri spravnom ukonceni fclose vracia 0 */
         printf("Subor sa nepodarilo zatvorit.\n");
         exit(1);
     }
@@ -124,8 +125,52 @@ void funkciaSOA3(Complex pole[], int dlzka_pola) {
 
 /* Teoreticka otazka: Vyberte si jednu z tem: RETAZEC, SUBOR, STRUKTURA alebo SMERNIK
  * (dalej X). Strucne definujte, co je to X, a uvedte na ake ulohy sa pouziva a akym
- * sposobom (v kontexte programovania v jazyku C). */
-/* TODO: napisat nejake omacky okolo... */
+ * sposobom (v kontexte programovania v jazyku C).
+ * RIESENIE
+ * RETAZEC - ide vlastne o postupnost znakov (typ char) ukoncenu znakom '\0', pouziva
+ * sa napriklad pri praci so subormi, ak potrebujeme nacitavat obsah suboru a dalej
+ * s nim pracovat, alebo pri vypisoch do konzoly.
+ * Priklady: retazec ako argument funkcie
+ * printf("FEI je skola hrou.\n");
+ * zadefinovanie a pouzitie retazca
+ * char retazec[51];
+ * fscanf(subor, "%50s", retazec);
+ * SUBOR - subor je vlastne postupnost bajtov, ulozena na disku. Pouziva sa napriklad
+ * na dlhodobe uchovanie dat.
+ * Priklady: otvorenie, zapis a zatvorenie suboru
+ * FILE *subor;
+ * subor = fopen("nazov.txt","w");
+ * fprintf(subor,"Toto si chcem ulozit do suboru...");
+ * fclose(subor);
+ * STRUKTURA - ide o zlozeny datovy typ, pouzivame ju, ak potrebujeme mat pokope viacero
+ * dat, napriklad ak chceme uchovavat zaznamy o knihach, tak strukturu moze tvorit autor,
+ * nazov knihy, rok vydania a pod...
+ * Priklady: zadefinovanie struktury
+ * typedef struct kniha{
+ * char nazov[100];
+ * char autor[50];
+ * int rok;
+ * } Kniha;
+ * Vytvorenie knihy a naplnenie udajmi
+ * Kniha k;
+ * k.nazov = "Zahada nevediaceho Cauchyho a Dalamberta";
+ * k.autor = "Boris Rudolf";
+ * k.rok = 1970;
+ * SMERNIK - je to premenna, v ktorej je ulozena adresa pamati, na ktoru ukazuje. 
+ * Znacime ho hviezdickou pred jeho menom. Pouziva sa napriklad na zmenu hodnot
+ * premennych vo funkciach, alebo ak potrebujeme dynamicky alokovanu pamat.
+ * Priklady: zmena hodnoty vo funkcii
+ * void funkcia(int *cislo) {
+ *      *cislo = *cislo+1;
+ * }
+ * volanie v inej funkcii potom bude vyzerat takto
+ * int a=10;
+ * funkcia(&a);
+ * po volani bude v a ulozena hodnota 11
+ * priradenie dynamicky alokovanej pamete
+ * int *cisla;
+ * cisla = malloc(sizeof(int)*10);
+ * s cislami mozme potom pracovat ako s polom o velkosti 10 prvkov */
 
 /* testovaci main */
 int main() {
